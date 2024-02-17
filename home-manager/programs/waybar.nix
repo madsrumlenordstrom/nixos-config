55ef79d00ae0aff4f8ae2b280df8e880edb558f9
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2024-02-17 20:43:18 +0100

    removed cringe binary workspace names

--- home-manager/programs/waybar.nix
+++ home-manager/programs/waybar.nix
@@ -12,15 +12,17 @@
     settings.main = { # Needs some arbitrary name
       layer = "top";
       position = "top";
-      modules-left = [ "sway/mode" "sway/workspaces" ];
-      modules-center = [ "clock" ];
-      modules-right = [ "network" "wireplumber" "battery" "sway/language" ];
       height = 27;
       margin-top = 6;
       margin-left = 44; # Fixes my broken screen
       margin-right = 6;
       margin-bottom = 0;
       spacing = 2;
+
+      modules-left = [ "sway/mode" "sway/workspaces" ];
+      modules-center = [ "clock" ];
+      modules-right = [ "network" "wireplumber" "battery" "sway/language" ];
+
       "sway/window" = { "max-length" = 50; };
       "sway/workspaces" = {
         "disable-scroll" = true;
@@ -28,16 +30,15 @@
         "disable-markup" = false;
         "format" = "{icon}";
         "format-icons" = {
-          "1" = "0001";
-          "2" = "0010";
-          "3" = "0011";
-          "4" = "0100";
-          "5" = "0101";
-          "6" = "0110";
-          "7" = "0111";
-          "8" = "1000";
-          "9" = "1001";
-          "10" = "1010";
+          "1" = "1";
+          "2" = "2";
+          "3" = "3";
+          "4" = "4";
+          "5" = "5";
+          "6" = "6";
+          "7" = "7";
+          "8" = "8";
+          "9" = "9";
         };
         "persistent-workspaces" = {
           "1" = [];
@@ -47,18 +48,21 @@
           "5" = [];
         };
       };
+
       "sway/mode" = { "format" = "{}"; };
+
       "network" = {
         "format-wifi" = " {signalStrength:3}%";
         "format-ethernet" = "{ifname} = {ipaddr}/{cidr}";
         "format-linked" = "{ifname} (No IP)";
         "format-disconnected" = "󰤯  {signalStrength:3}%";
       };
+
       "wireplumber" = {
         "format" = " {volume:3}%";
         "format-muted" = "󰖁 {volume:3}%";
-        "on-click" = "helvum";
       };
+
       "battery" = {
         "bat" = "BAT0";
         "adapter" = "ADP1";
@@ -72,13 +76,16 @@
         "format-icons" = [ "" "" "" "" "" ];
         "interval" = 10;
       };
+
       "clock" = {
         "format" = " {:%H:%M}";
         "format-alt" = " {:%Y-%m-%d %H:%M}";
         "interval" = 60;
       };
+
       "sway/language" = { "format" = "  {short}"; };
     };
+
     style = with config.colorScheme.palette; /*css*/ ''
       * {
       	border: 2px;
@@ -103,35 +110,20 @@
       #workspaces button {
       	color: #${base05};
       	background: #${base00};
-      	padding: 1px 15px 1px 10px;
+      	padding: 1px 5px 1px 5px;
       }
 
-      #workspaces button.focused:hover {
-      	color: #${base05};
+      #workspaces button.hover {
+      	background-color: #${base05};
       }
 
       #workspaces button.urgent {
-      	background-color: #${base0C};
+      	background-color: #${base08};
       }
 
-      #workspaces button#sway-workspace-1.focused {
-      	color: #${base08};
-      }
-
-      #workspaces button#sway-workspace-2.focused {
-      	color: #${base0B};
-      }
-
-      #workspaces button#sway-workspace-3.focused {
-      	color: #${base0A};
-      }
-
-      #workspaces button#sway-workspace-4.focused {
-      	color: #${base0D};
-      }
-
-      #workspaces button#sway-workspace-5.focused {
-      	color: #${base0E};
+      #workspaces button.focused {
+      	color: #${base05};
+      	background-color: #${base01};
       }
 
       .modules-left>widget:first-child>#workspaces {
