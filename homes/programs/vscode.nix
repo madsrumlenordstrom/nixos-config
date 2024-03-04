0ddcc9550563597568b5c95528d0fea1e8552513
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2024-03-04 21:05:30 +0100

    added nix IDE to vscode

--- homes/programs/vscode.nix
+++ homes/programs/vscode.nix
@@ -10,11 +10,29 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
+
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+    mutableExtensionsDir = false;
+
     extensions = with pkgs.vscode-extensions; [
       # Scala/Chisel
       scalameta.metals
       scala-lang.scala
+
+      # Nix
+      jnoortheen.nix-ide
     ];
-    mutableExtensionsDir = false;
+
+    userSettings = {
+      "telemetry.telemetryLevel" = "off";
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      };
   };
+
+  home.packages = with pkgs; [
+    # For Scala/Chisel
+    jdk
+  ];
 }
