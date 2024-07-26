97e57cfeb5f2eee2bf88263b30741ee0506050ab
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2024-07-26 12:13:10 +0200

    updated pkgs

---
+++ shared/edb/default.nix
@@ -0,0 +1,12 @@
+{ inputs, outputs, lib, config, pkgs, ... }:
+{
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
+  ];
+
+  # Global color scheme. See https://github.com/tinted-theming/base16-schemes
+  colorScheme = inputs.nix-colors.colorSchemes.catppuccin-frappe;
+
+  # Enable XDG base directories management
+  nix.settings.use-xdg-base-directories = true;
+}
