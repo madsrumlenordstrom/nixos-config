128433d692b81e3c4a5e10d16d3ec464dfda1653
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-01-08 23:35:49 +0100

    removed LC_TIME config

--- modules/nixos/i18n/default.nix
+++ modules/nixos/i18n/default.nix
@@ -11,8 +11,6 @@ in
   config = mkIf cfg.enable {
     i18n = { 
       defaultLocale = "en_US.UTF-8";
-      # Use ISO 8601 date format
-      extraLocaleSettings.LC_TIME = "en_DK.UTF-8";
       supportedLocales = lib.mkDefault [
         "en_US.UTF-8/UTF-8"
         "da_DK.UTF-8/UTF-8"
