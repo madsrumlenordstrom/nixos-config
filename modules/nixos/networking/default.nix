6b3eacb4f76a478a0f0c0b11d00903957a1afdf0
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-03-12 21:54:48 +0100

    switched to networkd

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -9,15 +9,35 @@ in
   options.networking.enable = mkEnableOption "Networking settings";
 
   config = mkIf cfg.enable {
+    systemd.network = {
+      enable = true;
+      wait-online.anyInterface = true;
+      wait-online.timeout = 20;
+      networks = {
+        "25-wireless" = {
+          matchConfig.Name = "wlp3s0";
+          linkConfig.RequiredForOnline = "routable";
+          networkConfig = {
+            DHCP = "yes";
+            IgnoreCarrierLoss = "3s";
+            DNSOverTLS = "opportunistic";
+          };
+        };
+      };
+      # Use predictable naming scheme
+      links."80-iwd".linkConfig.NamePolicy = mkForce "database onboard slot path mac keep kernel";
+    };
+
     networking = {
-      # Use iwd instead of wpa_supplicant
-      wireless.iwd.enable =  true;
-      networkmanager.enable = true;
-      networkmanager.wifi.backend = "iwd";
+      # DHCP managed by networkd
+      useDHCP = false;
 
       # Use nftables instead of legacy iptables
       nftables.enable = true;
       firewall.enable = true;
+
+      # Wireless daemon
+      wireless.iwd.enable = true;
     };
   };
 }
