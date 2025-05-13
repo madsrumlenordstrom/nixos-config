57d2cd0418758f7086e766b496fbbea09f81c603
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-05-14 00:41:26 +0200

    updated nix settings

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -7,15 +7,20 @@ let
 in
 {
   config = mkIf cfg.enable {
-    nix.settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = [ "nix-command" "flakes" ];
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-      # Use XDG directories instead of polluting home
-      use-xdg-base-directories = true;
-      # Add root and wheel group to trusted users
-      trusted-users = [ "root" "@wheel" ];
+    nix = {
+      settings = {
+        # Enable flakes and new 'nix' command
+        experimental-features = [ "nix-command" "flakes" ];
+        # Deduplicate and optimize nix store
+        auto-optimise-store = true;
+        # Use XDG directories instead of polluting home
+        use-xdg-base-directories = true;
+        # Add root and wheel group to trusted users
+        trusted-users = [ "@wheel" ];
+      };
+
+      # Only use CPU when available
+      daemonCPUSchedPolicy = "idle";
     };
   };
 }
